c7c30ca5846859b74eb7aa2ec25ef1e3d2e36020
Lucas Dantas <55062200+LucasDants@users.noreply.github.com>
2023-09-21 21:52:45 -0300

    fix: remove image requirement to update product

--- src/modules/products/useCases/updateProduct/UpdateProductController.ts
+++ src/modules/products/useCases/updateProduct/UpdateProductController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { container } from "tsyringe";
 
 import { UpdateProductUseCase } from "./UpdateProductUseCase";
+import { Product } from "@prisma/client";
 
 export class UpdateProductController {
   async execute(req: Request, res: Response) {
@@ -12,13 +13,18 @@ export class UpdateProductController {
     const updateProduct = container.resolve(UpdateProductUseCase);
     const requestImage = req.file as Express.Multer.File;
 
-    const product = await updateProduct.execute(productId, {
+    const newProduct = {
       title,
       description,
       price: Number(price),
-      imageURL: requestImage.filename,
       supplierId: accountId,
-    });
+    } as Product;
+
+    if (req.file?.filename) {
+      newProduct.imageURL = requestImage.filename;
+    }
+
+    const product = await updateProduct.execute(productId, newProduct);
 
     return res.status(200).send(product);
   }
